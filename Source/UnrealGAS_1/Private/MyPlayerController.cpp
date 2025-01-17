8244c07b684be2556f23fc81f3c277f18e0eb50d
정찬호_JCH <elvpfhhrm@gmail.com>
2025-01-17 16:16:24 +0900

    0117_clietn에서 Hud 출력 안 되는 것 고치기 실패

--- Source/UnrealGAS_1/Private/MyPlayerController.cpp
+++ Source/UnrealGAS_1/Private/MyPlayerController.cpp
@@ -9,6 +9,14 @@ void AMyPlayerController::OnPossess(APawn* aPawn)
 {
 	Super::OnPossess(aPawn);
 
+	if (!IsValid(aPawn))
+	{
+		GetWorld()->GetTimerManager().SetTimerForNextTick([this, aPawn]()
+			{
+				OnPossess(aPawn);
+			});
+		return;
+	}
 	AHUD* Hud = GetHUD();
 	if (IsValid(Hud))
 	{
@@ -18,11 +26,16 @@ void AMyPlayerController::OnPossess(APawn* aPawn)
 			// if it isn't my own controller, don't create it.
 			if (!IsLocalController())
 			{
+				UE_LOG(LogTemp, Warning, TEXT("Is not local controller"));
 				return;
 			}
 
 			MyHud->CreateHUD();
 			MyHud->SpawnPlayerStateSetting(Cast<AUnrealGAS_1Character>(aPawn));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("HUD is not valid in OnPossess"));
 		}
 	}
 }
@@ -41,10 +54,26 @@ void AMyPlayerController::OnRep_PlayerState()
 			// if it isn't my own controller, don't create it.
 			if (!IsLocalController())
 			{
+				UE_LOG(LogTemp, Warning, TEXT("Is not local controller"));
 				return;
 			}
+			APawn* MyPawn = GetCharacter();
+			if (!IsValid(MyPawn))
+			{
+				UE_LOG(LogTemp, Warning, TEXT("Pawn is not valid in OnRep_PlayerState, delaying..."));
 
-			MyHud->SpawnPlayerStateSetting(Cast<AUnrealGAS_1Character>(GetCharacter()));
+				// Pawn이 유효하지 않은 경우 일정 시간 후 다시 확인
+				GetWorld()->GetTimerManager().SetTimerForNextTick([this]()
+					{
+						OnRep_PlayerState();
+					});
+				return;
+			}
+			MyHud->SpawnPlayerStateSetting(Cast<AUnrealGAS_1Character>(MyPawn));
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("HUD is not valid in OnRep_PlayerState"));
 		}
 	}
 }
