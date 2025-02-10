cbd99dcceb188d3746dc80406134b579c54e483e
정찬호_JCH <elvpfhhrm@gmail.com>
2025-02-10 16:45:39 +0900

    commit

--- Source/UnrealGAS_1/Private/MyPlayerController.cpp
+++ Source/UnrealGAS_1/Private/MyPlayerController.cpp
@@ -1,57 +1,40 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "MyPlayerController.h"
 #include "MyHUD.h"
+#include "HUDWidget.h"
 #include "UnrealGAS_1\UnrealGAS_1Character.h"
 
 void AMyPlayerController::OnPossess(APawn* aPawn)
 {
 	Super::OnPossess(aPawn);
 	
-	// if it isn't my own controller, don't create it.
-	if (!IsLocalPlayerController())
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Is not local controller"));
-		return;
-	}
-
-	AHUD* Hud = GetHUD();
-	if (IsValid(Hud))
-	{
-		AMyHUD* MyHud = Cast<AMyHUD>(Hud);
-		if (IsValid(MyHud))
-		{
-				MyHud->CreateHUD();
-
-			MyHud->SpawnPlayerStateSetting(Cast<AUnrealGAS_1Character>(aPawn));
-		}
-		else
-		{
-			UE_LOG(LogTemp, Warning, TEXT("HUD is not valid in OnPossess"));
-		}
-	}
+	GetHudTryCnt = 0;
+	SetHudWidget();
 }
 
 void AMyPlayerController::OnRep_PlayerState()
 {
 	Super::OnRep_PlayerState();
 
+	GetHudTryCnt = 0;
+	SetHudWidget();
+}
 
+void AMyPlayerController::SetHudWidget()
+{
+	// if it isn't my own controller, don't create it.
+	if (!IsLocalPlayerController())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Is not local controller"));
+		return;
+	}
 	AHUD* Hud = GetHUD();
 	if (IsValid(Hud))
 	{
 		AMyHUD* MyHud = Cast<AMyHUD>(Hud);
 		if (IsValid(MyHud))
 		{
-			// if it isn't my own controller, don't create it.
-			if (!IsLocalPlayerController())
-			{
-				UE_LOG(LogTemp, Warning, TEXT("Is not local controller"));
-				return;
-			}
 			APawn* MyPawn = GetCharacter();
-			
+
 			MyHud->CreateHUD();
 			MyHud->SpawnPlayerStateSetting(Cast<AUnrealGAS_1Character>(MyPawn));
 		}
@@ -60,4 +43,50 @@ void AMyPlayerController::OnRep_PlayerState()
 			UE_LOG(LogTemp, Warning, TEXT("HUD is not valid in OnRep_PlayerState"));
 		}
 	}
+	else
+	{
+		if (GetHudTryCnt++ < 20)
+		{
+			GetWorld()->GetTimerManager().SetTimer(TimerForGetHUD, this, &AMyPlayerController::SetHudWidget, 0.2f, false, 0.2f);
+		}
+	}
 }
+
+//void AMyPlayerController::CreateHUD()
+//{
+//	// if it already exsit, don't operate.
+//	if (UIHUDWidget != nullptr)
+//	{
+//		return;
+//	}
+//
+//	// Must have origin class, which put in the editor.
+//	if (UIHudWidgetClass == nullptr)
+//	{
+//		// Show Error msg with Function name.
+//		UE_LOG(LogTemp, Error, TEXT("%s() Missing UIHUDWidgetClass."), *FString(__FUNCTION__));
+//		return;
+//	}
+//
+//	// Create widget
+//	UIHUDWidget = CreateWidget<UHUDWidget>(GetWorld(), UIHudWidgetClass);
+//	// Put it in a viewport and print it out on the screen
+//	UIHUDWidget->AddToViewport();
+//
+//}
+//
+//UHUDWidget* AMyPlayerController::GetHUD()
+//{
+//	return UIHUDWidget;
+//}
+//
+//void AMyPlayerController::SpawnPlayerStateSetting(AUnrealGAS_1Character* player)
+//{
+//	if (player && UIHUDWidget)
+//	{
+//		UIHUDWidget->SetPlayer(player);
+//		UIHUDWidget->SetCurrentHealth(player->GetHealth());
+//		UIHUDWidget->SetMaxHealth(player->GetMaxHealth());
+//	}
+//}
+
--- Source/UnrealGAS_1/Public/MyPlayerController.h
+++ Source/UnrealGAS_1/Public/MyPlayerController.h
@@ -17,5 +17,25 @@ class UNREALGAS_1_API AMyPlayerController : public APlayerController
 	// Execute When Poseess
 	virtual void OnPossess(APawn* aPawn) override;
 	// Execute Whe Player State Changed
-	virtual void OnRep_PlayerState() override;
+	virtual void OnRep_PlayerState() override; 
+
+private:
+	void SetHudWidget();
+//protected:
+//	//에디터에서 TSubclassOf 해당 자료형 클래스만 드래그 가능하게
+//	//우리가 에디터에 넣어줄 UI클래스 원본.
+//	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "RPG|UI")
+//	TSubclassOf<class UHUDWidget> UIHUDWidgetClass;
+//
+//	UPROPERTY(BlueprintReadWrite, Category = "RPG|UI")
+//	class UHUDWidget* UIHUDWidget;//원본 기준으로 생성된 UI
+//
+//public:
+//	class UHUDWidget* GetHUD();
+//	void CreateHUD();
+//
+//	UFUNCTION()
+//	void SpawnPlayerStateSetting(class AUnrealGAS_1Character* _Player);
+	FTimerHandle TimerForGetHUD;
+	float GetHudTryCnt;
 };
